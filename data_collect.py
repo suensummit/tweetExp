e56c3a57a1c04c34e68d0abf93282e5d0469a04b
Summit Suen <summit.suen@gmail.com>
2015-03-18 15:50:14 +0800

    init 2015-03-18

---
+++ data_collect.py
@@ -0,0 +1,44 @@
+__author__ = 'kimi&summit'
+
+from twitter import *
+import time
+import dateutil.parser
+from elasticsearch import Elasticsearch
+
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+ACCESS_KEY = ""
+ACCESS_SECRET = ""
+
+es = Elasticsearch("104.236.193.82:9200")
+
+# create twitter API object
+auth = OAuth(ACCESS_KEY, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+stream = TwitterStream(auth = auth, secure = True)
+
+# iterate over tweets matching this filter text
+# IMPORTANT! this is not quite the same as a standard twitter search
+#  - see https://dev.twitter.com/docs/streaming-api
+tweet_iter = stream.statuses.filter(track = "oil")
+
+for tweet in tweet_iter:
+    # check whether this is a valid tweet
+    if tweet.get('text'):
+        # yes it is! print out the contents, and any URLs found inside
+        print "(%s) @%s %s" % (tweet["created_at"], tweet["user"]["screen_name"], tweet["text"])
+        ts = time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(tweet['created_at'],'%a %b %d %H:%M:%S +0000 %Y'))
+        ts = dateutil.parser.parse(ts)
+        ts = ts.isoformat()
+        print(ts)
+
+        res = es.index(
+            index="twi-mlb", doc_type=ts, id=ts, body={
+                'timestamp':ts,
+                'text':tweet["text"]
+            })
+
+        #x = tweet["text"].split()
+        #print x
+
+        #for url in tweet["entities"]["urls"]:
+        #    print " - found URL: %s" % url["expanded_url"]
